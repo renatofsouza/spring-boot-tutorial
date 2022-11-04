794c54b7fd6a00740ba7a67c70401ec9eb7ab926
renato Souza <renato.souza@gmail.com>
2022-11-04 18:51:06 -0400

    Add example of how to use default configuration file property value.

--- src/main/java/com/rs/springboot/tutorial/controller/HelloController.java
+++ src/main/java/com/rs/springboot/tutorial/controller/HelloController.java
@@ -1,13 +1,16 @@
 package com.rs.springboot.tutorial.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${welcome.message}")
+    private String welcomeMessage;
     @GetMapping("/")
     public String helloWorld(){
-        return "Welcome to Spring Boot Tutorial!";
+        return welcomeMessage;
     }
 }
